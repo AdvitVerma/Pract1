e27ab7206cf62b1310ed6aeb00f9137f51d80892
Advit <Advit@192.168.0.19>
2020-02-20 12:27:24 -0500

    test com

---
+++ src/gitpractice1/GitPractice1.java
@@ -0,0 +1,21 @@
+
+package gitpractice1;
+
+/**
+ *
+ * @author Advit
+ */
+public class GitPractice1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("Hello World");
+        
+    }
+    
+}
